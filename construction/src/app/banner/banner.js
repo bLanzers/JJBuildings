6b6600168f986231608be26151fcbc4a65655025
ilanzerdev <jerinmanickam@gmail.com>
2021-02-24 22:03:41 +0530

    initial commit

---
+++ construction/src/app/banner/banner.js
@@ -0,0 +1,14 @@
+
+ // import Swiper JS
+// import Swiper from 'swiper';
+// import $ from "jquery";
+// import './popper.js';
+// import Swiper styles
+// import 'swiper/swiper-bundle.css';
+  import Splide from '@splidejs/splide';
+  import '@splidejs/splide/dist/css/splide-core.min.css';
+  import '@splidejs/splide/dist/css/splide.min.css';
+
+$(document).ready(function(){
+new Splide( '#splide' ).mount();
+});
